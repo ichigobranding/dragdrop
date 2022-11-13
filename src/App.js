263557017e7c0b8469b7fd9cca5521c6c18fc5ff
Soichiro Tanaka <tanaka-pc@SoichironoMacBook-Pro.local>
2022-11-13 23:00:16 +0900

    不要なもの削除

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import "./App.css";
 function App() {
   const [items] = useState(["item0", "item1", "item2"]);
   const onDragEnd = (result) => {
-    console.log(result.source.index);
-    console.log(result.destination.index);
+    //console.log(result.source.index);
+    //console.log(result.destination.index);
     const removed = items.splice(result.source.index, 1); //スタート地点を消す
-    console.log(removed[0]);
+    //console.log(removed[0]);
     const check = items.splice(result.destination.index, 0, removed[0]); // エンド地点に消したものを追加
-    console.log(check);
+    //console.log(check);
   }
   return (
     <div className="dragDropArea">
